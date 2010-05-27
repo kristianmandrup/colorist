7cb5b4761c38bceaddfb664141f81f13b3a782d8
Kristian Mandrup <kmandrup@gmail.com>
2010-05-27 15:10:44 -0400

    extracted tests

--- lib/colorist/color_names.rb
+++ lib/colorist/color_names.rb
@@ -423,19 +423,4 @@ module Colorist
   end
 end
 
-# TESTING
-
-puts Colorist::ColorNames.color('0xfff', :lower)                                                              
-# puts x = Colorist::ColorNames.color(:'#FFFFAA', :lower)                                                              
-# puts x = Colorist::ColorNames.color('0xFFFFAA', [:hex, :lower])                                                              
-# puts x = Colorist::ColorNames.color(:red, :hex, :lower)                                                              
-# puts x = Colorist::ColorNames.color('Coconut', [:hex, :lower])
-#puts Colorist::ColorNames.to_color(x)                                                                                       
-
-# puts Colorist::ColorNames.to_color('coconut')
-# 
-# # can't be found
-puts Colorist::ColorNames.to_color('#FFF')
-# puts Colorist::ColorNames.color('hello', [:hex, :lower])
-
 
